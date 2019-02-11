a3264bc545a55eee6b85ddf53f3f559bcccf23fc
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-02-11 23:31:49 +0100

    Part 2 of Day 12

--- Day12/index.js
+++ Day12/index.js
@@ -12,40 +12,119 @@ rules = rules.split('\r\n').map( r => {
   return rule
 })
 
-const DEQ = new DoubleEndedQueue()
+const DEQ = new DoubleEndedQueue(false)
 
 for (let i = 0; i < initialState.length; i++) {
   const pot = initialState[i];
   DEQ.push({
-    containsPlant: pot === '#',
+    content: pot,
     index: i
   })
 }
 
-// add 5 empty pots to beginning of DEQ
-for (let i = 0; i < 5; i++) {
-  const index = DEQ.first.value.index - 1
-  DEQ.unshift({
-    containsPlant: false,
-    index
-  })
+// Part 1
+// const sum = calculateSum(20)
+// console.log(sum)
+
+// Part 2
+// The sum repetitively increases by 23 after generation 162
+// Solution = sum + (50000000000 - 162) * 23
+const sum = calculateSum(162) + (50000000000 - 162) * 23
+console.log(sum)
+
+
+
+function calculateSum(generations) {
+  const padding = 3
+  let previousSum = 0
+
+  for (let i = 0; i < generations; i++) {
+    // add 5 empty pots to beginning of DEQ
+    for (let i = 0; i < padding; i++) {
+      const index = DEQ.first.value.index - 1
+      DEQ.unshift({
+        content: '.',
+        index
+      })
+    }
+
+    // add 5 empty pots to end of DEQ
+    for (let i = 0; i < padding; i++) {
+      const index = DEQ.last.value.index + 1
+      DEQ.push({
+        content: '.',
+        index
+      })
+    } 
+    
+    const modifications = []
+    for (let r = 0; r < rules.length; r++) {
+      const rule = rules[r];
+      
+      for (let j = 0; j < DEQ.length - 2; j++) {
+        const currentPot = DEQ.first.next.next
+        let temp = ''
+
+        temp += currentPot.pre.pre.value.content
+        temp += currentPot.pre.value.content
+        temp += currentPot.value.content
+        temp += currentPot.next.value.content
+        temp += currentPot.next.next.value.content
+
+        if (temp === rule.predicate) {
+          modifications.push({ index: currentPot.value.index, mod: rule.result })
+        }
+        
+        DEQ.rotate(-1)
+      }
+      DEQ.rotate(-2) // get queue back to original order after every rule
+    }
+
+    for (let j = 0; j < DEQ.length; j++) {
+      const m = modifications.find( m => m.index === DEQ.first.value.index )
+      if (m) {
+        DEQ.first.value.content = m.mod
+      }
+      DEQ.rotate(-1)
+    }
+
+    // let sum = 0
+    // for (let i = 0; i < DEQ.length; i++) {
+    //   if (DEQ.first.value.content === '#') {
+    //     sum += DEQ.first.value.index
+    //   }
+    //   DEQ.rotate(-1)
+    // }
+
+    // console.log('Generation ' + i)
+    // console.log('Current sum ' + sum)
+    // console.log('Difference to previous sum ' + (sum - previousSum))
+
+    // previousSum = sum
+  }
+
+  let sum = 0
+  for (let i = 0; i < DEQ.length; i++) {
+    if (DEQ.first.value.content === '#') {
+      sum += DEQ.first.value.index
+    }
+    DEQ.rotate(-1)
+  }
+
+  return sum
 }
 
-// add 5 empty pots to end of DEQ
-for (let i = 0; i < 5; i++) {
-  const index = DEQ.last.value.index + 1
-  DEQ.push({
-    containsPlant: false,
-    index
-  })
-}
+
+
+
+
 
 // console.log(DEQ.first)
 // console.log(DEQ.last)
 // console.log(DEQ.length)
 
-
-const generations = 50000000000
+/*
+const generations = 20
 const padding = 50
 let currentState = initialState
 for (let i = 0; i < generations; i++) {
@@ -77,3 +156,4 @@ for (let i = 0; i < currentState.length; i++) {
 }
 
 console.log(sum)
+*/
