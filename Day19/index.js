336bff468831d9a556a9f21d28e27496a48f71d3
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-02-23 15:12:33 +0100

    Day 19 Part 2

--- Day19/index.js
+++ Day19/index.js
@@ -7,21 +7,39 @@ const ipRegisterIndex = parseFloat(ipInstruction.substring(ipInstruction.length-
 
 const registers = Array.from('0'.repeat(6)).map(parseFloat)
 
-let nextInstruction = instructions[registers[ipRegisterIndex]]
-
 const operations = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
 
-while (nextInstruction !== undefined) {
+while (true) {
+
+  const nextInstruction = instructions[registers[ipRegisterIndex]]
+  if (nextInstruction == undefined) {
+    break
+  }
+
+  // console.log(registers[ipRegisterIndex], nextInstruction, registers)
   const [opcode, ...input] = nextInstruction.split(' ')
   const op = operations.find( op => op.name === opcode)
   
   op(registers, input.map(parseFloat))
 
   registers[ipRegisterIndex]++
-  nextInstruction = instructions[registers[ipRegisterIndex]]
 }
 
 console.log(registers)
+
+// Part 2
+
+// After very very long detailed inspection of opcode operations
+// I figured out that the program sums up all integers which
+// divide a target number (10551381) with a remainder of 0
+let sum = 0
+const target = 10551381
+for (let i = 0; i < 10551381 + 1; i++) {
+  if (target % i == 0) {
+    sum+=i
+  }
+}
+console.log(sum)
 
 
 function addr(registers, input) {
